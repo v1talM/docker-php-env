9a858b4273bbdf06d8ea4488c685b2a57de2d561
vital <373357042@qq.com>
2018-11-12 22:06:22 +0800

    安装脚本更新

--- install.sh
+++ install.sh
@@ -6,6 +6,9 @@ log_path='log'
 # app根目录
 app_path='app'
 
+# service根目录
+service_path='service'
+
 # nginx日志文件路径
 nginx_log_path=$log_path/nginx
 
@@ -13,7 +16,7 @@ nginx_log_path=$log_path/nginx
 php_log_path=$log_path/php
 
 # 创建相关日志文件
-mkdir -p $log_path/nginx  $log_path/php  $app_path
-touch $nginx_log_path/access_log  $nginx_log_path/error_log
+mkdir -p $log_path/nginx  $log_path/php  $app_path  $service_path/nginx/vhost  $service_path/mysql/data
+touch $nginx_log_path/access.log  $nginx_log_path/error.log
 
 echo "install complete!"
